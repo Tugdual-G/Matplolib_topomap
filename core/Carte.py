0140638f9711085937912f2477fb14293a4d2503
Tugdual <tugducarfantan@gmail.com>
2023-01-20 10:57:38 +0100

    object

---
+++ core/Carte.py
@@ -0,0 +1,75 @@
+#!/usr/bin/env python3
+from core.select_raster import select_raster
+from core.select_shape import select_shape_data
+import pandas as pd
+import numpy as np
+
+
+class Carte:
+    def __init__(self, center, margin):
+
+        self.x0, self.y0 = center
+        self.margin = margin
+        left = self.x0 - margin
+        right = self.x0 + margin
+        bottom = self.y0 - margin
+        top = self.y0 + margin
+        self.bounds = (left, bottom, right, top)
+
+        self.crs = "epsg:2154"
+
+    # stylesheet specific to osm data fclass
+    def set_style(self, path):
+        style = pd.read_csv(path)
+        # TODO modify style sheet to add a txt column
+        self.style = style[
+            (style["use"] == True) & (style["category"] != "places")
+        ].set_index("fclass")
+        self.txt_style = style[
+            (style["use"] == True) & (style["category"] == "places")
+        ].set_index("fclass")
+
+    def set_raster_data(self, paths, step=1):
+        self.raster_data, self.X, self.Y = select_raster(self.bounds, paths, step=step)
+
+    def set_shape_data(self, paths):
+        # extract data in area of interest
+        df = select_shape_data(self.bounds, paths)
+
+        # stitch into one dataframe
+        self.shape_df = df[
+            df["fclass"].isin(self.style.index.tolist() + self.txt_style.index.tolist())
+        ]
+        self.shape_df = self.shape_df.to_crs(self.crs)
+
+    def plot_shapes(self, ax):
+
+        for fclass in self.style.index.tolist():
+            styledict = eval(self.style.loc[fclass]["style"])
+            self.shape_df[self.shape_df["fclass"] == fclass].plot(ax=ax, **styledict)
+
+    def plot_txt(self, ax):
+
+        txtmargin = self.margin * 0.8
+        txtleft = self.x0 - txtmargin
+        txtright = self.x0 + txtmargin
+        txtbottom = self.y0 - txtmargin
+        txttop = self.y0 + txtmargin
+
+        for fclass in self.txt_style.index.tolist():
+            styledict = eval(self.txt_style.loc[fclass]["style"])
+            txt_df = self.shape_df.loc[self.shape_df["fclass"] == fclass].cx[
+                txtleft:txtright, txtbottom:txttop
+            ]
+            for name in txt_df["name"]:
+                xt = txt_df[txt_df["name"] == name]["geometry"].centroid.x.iloc[0]
+                yt = txt_df[txt_df["name"] == name]["geometry"].centroid.y.iloc[0]
+                ax.text(
+                    xt,
+                    yt,
+                    s=name,
+                    horizontalalignment="center",
+                    verticalalignment="center",
+                    bbox=dict(boxstyle="round", alpha=0.4, color="w"),
+                    **styledict,
+                )
--- plot_map.py
+++ plot_map.py
@@ -1,12 +1,11 @@
 #!/usr/bin/env python3
 import pandas as pd
 import matplotlib.pyplot as plt
-from core.select_shape import select_shape_data
-from core.select_raster import select_raster
 import numpy as np
 from matplotlib.colors import LightSource
 import glob
 from rasterio.plot import show
+from core.Carte import Carte
 
 # Window, area of interest from center coordinates x0, y0
 # projection lambert93
@@ -20,14 +19,13 @@ bottom = y0 - margin
 top = y0 + margin
 bounds = (left, bottom, right, top)
 
+# Creatin the map object
+carte = Carte((x0, y0), margin)
+
 # stylesheet specific to osm data fclass
-style0 = pd.read_csv("map_style.csv")
-style = style0[(style0["use"] == True) & (style0["category"] != "places")].set_index(
-    "fclass"
-)
-txt_style = style0[
-    (style0["use"] == True) & (style0["category"] == "places")
-].set_index("fclass")
+style_path = "map_style.csv"
+carte.set_style(style_path)
+
 
 # data directory and files path
 dir_path1 = "data/shapes/osm_pays_loire"
@@ -41,11 +39,7 @@ dir_path2 = "data/shapes/osm_poitou-charente"
 shapes_paths += [(dir_path2 + name) for name in filesnames]
 
 # extract data in area of interest
-df = select_shape_data(bounds, shapes_paths)
-#
-# stitch into one dataframe
-df = df[df["fclass"].isin(style.index.tolist() + txt_style.index.tolist())]
-df = df.to_crs("EPSG:2154")
+carte.set_shape_data(shapes_paths)
 
 
 # raster data
@@ -53,55 +47,32 @@ dir_name1 = "data/raster/BDALTIV2_2-0_25M_ASC_LAMB93-IGN69_D085_2021-09-15/BDALT
 dir_name2 = "data/raster/BDALTIV2_2-0_25M_ASC_LAMB93-IGN69_D079_2022-10-05/BDALTIV2/1_DONNEES_LIVRAISON_2022-12-00016/BDALTIV2_MNT_25M_ASC_LAMB93_IGN69_D079"
 f_paths = glob.glob(dir_name1 + "/*.asc")
 f_paths += glob.glob(dir_name2 + "/*.asc")
-data, x, y = select_raster(bounds, f_paths, step=step)
+
+carte.set_raster_data(f_paths, step)
 
 
 # PLOT
 print("generating figure")
 fig, ax = plt.subplots(frameon=False)
 
+carte.plot_shapes(ax)
+carte.plot_txt(ax)
 
-def plot_shapes(ax):
-    for fclass in style.index.tolist():
-        styledict = eval(style.loc[fclass]["style"])
-        df[df["fclass"] == fclass].plot(ax=ax, **styledict)
+x, y = carte.X, carte.Y
+elev = carte.raster_data[0]
 
-    txtmargin = margin * 0.8
-    txtleft = x0 - txtmargin
-    txtright = x0 + txtmargin
-    txtbottom = y0 - txtmargin
-    txttop = y0 + txtmargin
-
-    for fclass in txt_style.index.tolist():
-        styledict = eval(txt_style.loc[fclass]["style"])
-        txt_df = df.loc[df["fclass"] == fclass].cx[txtleft:txtright, txtbottom:txttop]
-        for name in txt_df["name"]:
-            xt = txt_df[txt_df["name"] == name]["geometry"].centroid.x.iloc[0]
-            yt = txt_df[txt_df["name"] == name]["geometry"].centroid.y.iloc[0]
-            ax.text(
-                xt,
-                yt,
-                s=name,
-                horizontalalignment="center",
-                verticalalignment="center",
-                bbox=dict(boxstyle="round", alpha=0.4, color="w"),
-                **styledict,
-            )
-
-
-plot_shapes(ax)
 ls = LightSource(azdeg=100, altdeg=45)
 dx = 25 * step
-grayscale = ls.hillshade(data[0], dx=dx, dy=dx)
-ax.pcolormesh(x, y, grayscale, alpha=0.4, cmap="gray", shading="gouraud", zorder=20)
+grayscale = ls.hillshade(elev, dx=dx, dy=dx)
+black = np.zeros_like(grayscale)
+ax.pcolormesh(
+    x, y, black, alpha=0.3 * (1 - grayscale), cmap="gray", shading="gouraud", zorder=20
+)
+
 interlevels = np.arange(0, 300, 10)
-ax.contour(
-    x, y, data[0], colors="sienna", levels=interlevels, linewidths=0.5, zorder=18
-)
+ax.contour(x, y, elev, colors="sienna", levels=interlevels, linewidths=0.5, zorder=18)
 levels = np.arange(0, 300, 50)
-CS = ax.contour(
-    x, y, data[0], colors="sienna", levels=levels, linewidths=0.7, zorder=19
-)
+CS = ax.contour(x, y, elev, colors="sienna", levels=levels, linewidths=0.7, zorder=19)
 # plt.clabel(CS, fontsize=7, inline=False, inline_spacing=5)
 
 
